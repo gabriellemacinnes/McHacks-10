3c642d7019b19badcf29a26d0fc4a1b33ef5b536
Gabrielle MacInnes <gabrielle.macinnes@gmail.com>
2023-01-28 19:30:07 -0500

    scrape

--- scraping/popup.js
+++ scraping/popup.js
@@ -1,4 +1,4 @@
-document.getElementById("sum_text").addEventListener('click', get_text);
+document.getElementById("sum_text").addEventListener('click', getCurrentTab);
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     tabs[0].url;     //url
@@ -6,5 +6,30 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 });
 
 function get_text() {
-  document.getElementById('pagetitle').innerHTML = window.title
+  var hello = "some text"
+  document.getElementById('pagetitle').innerHTML = hello
 }
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  //document.getElementById('pagetitle').innerHTML = tab.url;
+  $.ajax({
+    url: "/cgi-bin/hello.py",
+    type: "POST",
+    data: {"text" : message},
+    success: function(response){
+            $("#div").html(response);
+    }
+});
+  return tab.url;
+}
+$.ajax({
+  type: "POST",
+  url: "scrape.py",
+  data: { param: text}
+  }).done(function(o) {
+      console.log(data);
+      console.log(text);
+  });
