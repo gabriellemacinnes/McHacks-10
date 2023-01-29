dff562a1b49f1c8b2c9200d2ad8c1669b52cc5fb
Gabrielle MacInnes <gabrielle.macinnes@gmail.com>
2023-01-29 04:05:33 -0500

    improving web scraping

--- research_extension/app/backend.py
+++ research_extension/app/backend.py
@@ -14,7 +14,17 @@ def tag_visible(element):
         return False
     return True
 
+def get_text(url):
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    main_soup = soup.find('main')
+    total = ""
+    for text in main_soup.find_all("p"):
+        total += text.get_text()
+    return total.split()
 
+'''
 def get_text(url):
     page = urlopen(url)
     html = page.read().decode("utf-8")
@@ -35,7 +45,7 @@ def divide_chunks(l, n):
     # looping till length l
     for i in range(0, len(l), n):
         yield l[i:i + n]
-
+'''
 
 def create_summary(l):
     divided = list(divide_chunks(l, 100))
