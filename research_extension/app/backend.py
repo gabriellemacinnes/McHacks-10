2b91208353625abdb2100f81136188b175beec43
Tessa Davis <tkdavis@eastlink.ca>
2023-01-29 00:52:09 -0500

    scraping mostly working

---
+++ research_extension/app/backend.py
@@ -0,0 +1,62 @@
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+
+from urllib.request import urlopen
+import cohere
+
+co = cohere.Client("o2KYh1CEVLYwS0ePRO4VmKsIWZuaSuz5cDS1MWjZ")
+
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def get_text(url):
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    total = u" ".join(t.strip() for t in visible_texts)
+
+    total_list = total.split()
+    return total_list
+
+    #text_p = (''.join(s.findAll(text=True)) for s in soup.findAll('p'))
+    #return text_p
+    #return soup.get_text()
+
+
+def divide_chunks(l, n):
+    # looping till length l
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def create_summary(l):
+    divided = list(divide_chunks(l, 100))
+    l_sum = []
+    for i in divided:
+        i = " ".join(i)
+        sum = summarize(i)
+        l_sum.append(sum)
+    total_sum = " ".join(l_sum)
+    return total_sum
+
+
+def summarize(string):
+    string = string + "\n In summary: \n --"
+
+    response = co.generate(
+        model='xlarge',
+        prompt=string,
+        max_tokens=40,
+        temperature=1,
+        stop_sequences=["--"])
+
+    summary = response.generations[0].text
+    return summary
--- scraping/popup.js
+++ scraping/popup.js
@@ -1,27 +1,26 @@
 document.getElementById("sum_text").addEventListener('click', getCurrentTab);
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    tabs[0].url;     //url
-    tabs[0].title;   //title
-});
 
-function get_text() {
-  var hello = "some text"
-  document.getElementById('pagetitle').innerHTML = hello
-}
 
 async function getCurrentTab() {
   let queryOptions = { active: true, lastFocusedWindow: true };
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
   let [tab] = await chrome.tabs.query(queryOptions);
-  document.getElementById('pagetitle').innerHTML = "hello";
-  $.ajax({
-    url: "../research_extension/app/views.py",
-    type: "POST",
-    data: {"text" : JSON.parse(tab.url)},
-    success: function() {document.getElementById('pagetitle').innerHTML = "success";},
-    failure: function() {document.getElementById('pagetitle').innerHTML = "failure";}
-  });
-  document.getElementById('pagetitle').innerHTML = tab.url;
+  $.post("http://127.0.0.1:8000/app/url_parse",
+    {
+      url: tab.url,
+    },
+    function(data, status){
+      alert("Data: " + data + "\nStatus: " + status);
+    });
+
+  document.getElementById('text').innerHTML = tab.url;
   return tab.url;
 }
+
+
+function get_text(value) {
+  var hello = "some text"
+  document.getElementById('text').innerHTML = url
+}
+
