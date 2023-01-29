4b66d12b1e3957785266785fba0ba5e0b66cf714
Gabrielle MacInnes <gabrielle.macinnes@gmail.com>
2023-01-29 01:21:05 -0500

    focus mode

--- combined_extension/background.js
+++ combined_extension/background.js
@@ -1,6 +1,40 @@
 const extensions = 'https://developer.chrome.com/docs/extensions'
 const webstore = 'https://developer.chrome.com/docs/webstore'
 
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.action.setBadgeText({
+		text: ""
+	});
+});
+
+chrome.commands.onCommand.addListener(async function(command) {
+	let queryOptions = { active: true, lastFocusedWindow: true };
+	let [tab] = await chrome.tabs.query(queryOptions);
+	// Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+	const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+	// Next state will always be the opposite
+	const nextState = prevState === 'Focus' ? '' : 'Focus'
+
+	// Set the action badge to the next state
+	await chrome.action.setBadgeText({
+	text: nextState,
+	});
+	if (nextState === "Focus") {
+	// Insert the CSS file when the user turns the extension on
+	await chrome.scripting.insertCSS({
+		files: ["focus.css"],
+		target: { tabId: tab.id },
+	});
+	} else if (nextState === "") {
+	// Remove the CSS file when the user turns the extension off
+	await chrome.scripting.removeCSS({
+		files: ["focus.css"],
+		target: { tabId: tab.id },
+	});
+	}
+    
+});
+
 var serverhost = 'http://127.0.0.1:8000';
 
 	chrome.runtime.onMessage.addListener(
@@ -20,3 +54,4 @@ var serverhost = 'http://127.0.0.1:8000';
 			return true;  // Will respond asynchronously.
 
 	});
+
--- combined_extension/content.js
+++ combined_extension/content.js
@@ -40,3 +40,4 @@ function getMarkerPosition() {
     display: "flex",
   };
 }
+
